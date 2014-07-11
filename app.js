fb0ca0b01d5536d8594877f8f9dd7540275871f6
Ye Lin Aung <me@yelinaung.com>
2014-07-11 10:00:41 +0630

    init commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express   = require('express');    // call express
+var app       = express();             // define our app using express
+var read      = require('node-read');
+$            = require('cheerio');
+
+port = process.env.PORT || 8080;    // set our port
+var router = express.Router();        // get an instance of the express Router
+
+router.get('/', function(req, res) {
+  read('http://programmingexcuses.com', function(err, article, response) {
+    res.json({ message: $('a', article.content).text() });
+  });
+});
+
+app.use('/api', router);
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+
