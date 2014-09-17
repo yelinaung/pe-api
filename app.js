a0e15b33c4fbe511f928f9f34dea45d4023591e9
Ye Lin Aung <me@yelinaung.com>
2014-09-17 11:55:53 +0630

    add a post route for gcm

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-var express   = require('express');     // call express
-var app       = express();              // define our app using express
-var read      = require('node-read'); 
-$             = require('cheerio');
+var express     = require('express');     // call express
+var app         = express();              // define our app using express
+var read        = require('node-read'); 
+$               = require('cheerio');
 
-port          = process.env.PORT || 8080;     // set our port
-var router    = express.Router();             // get an instance of the express Router
+port            = process.env.PORT || 8080;     // set our port
+var router      = express.Router();             // get an instance of the express Router
+var bodyParser  = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 router.get('/', function(req, res) {
   read('http://programmingexcuses.com', function(err, article, response) {
     res.json({ message: $('a', article.content).text() });
   });
+});
+
+router.post('/gcm', function(req, res) {
+  console.log("uuid -> " + req.body.uuid);
+  console.log("device id -> " + req.body.device_id);
+  res.end("res_sent");
 });
 
 app.use('/', router);
